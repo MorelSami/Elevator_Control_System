94795b96f031cd2c1df77817f35381c45db37c67
Morel Sami <mksami237@gmail.com>
2020-06-08 08:20:58 -0400

    unitTest.php update

--- public/unitTest.php
+++ public/unitTest.php
@@ -10,8 +10,8 @@
   *for testing purposes or you can use RestEasy/Postman which are commonly used  REST API Clients
   */
 
-
-    $elevator_request = {"startFloor":1, "stopFloor":5};  //json format
+    $data = array('startFloor' => 1, 'stopFloor'=> 6);
+    $elevator_request = json_encode($data);  //{"startFloor": 1, "stopFloor": 5};  //json format
     $url = 'http://localhost/EFI/elevator_test/public/index.php/elevatorRequest';
     $user_agent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0";
 
@@ -19,19 +19,12 @@
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch,CURLOPT_USERAGENT,$user_agent);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            "Accept: application/json"));
+            "Content-Type: application/json"));
     curl_setopt($ch, CURLOPT_POST, true); 
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_request);    
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($ch);
-        
-    if(!curl_errno($ch)){
-        echo "Good Job</br>";
-            
-     } else {
-        echo curl_error($ch)."</br>");
-     }
-      curl_close($ch);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $elevator_request);  
+    $response = curl_exec($ch);          
+
+    curl_close($ch);
 
 
   ?>
